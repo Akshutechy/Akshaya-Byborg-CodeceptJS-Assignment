5051f979d0598e174030a14473933b826507ebb3
akshutechy <akshaya.techy@gmail.com>
2024-11-16 15:22:57 +0530

    installed codeceptjs with playwright

---
+++ codecept.conf.js
@@ -0,0 +1,29 @@
+const { setHeadlessWhen, setCommonPlugins } = require('@codeceptjs/configure');
+// turn on headless mode when running with HEADLESS=true environment variable
+// export HEADLESS=true && npx codeceptjs run
+setHeadlessWhen(process.env.HEADLESS);
+
+// enable all common plugins https://github.com/codeceptjs/configure#setcommonplugins
+setCommonPlugins();
+
+/** @type {CodeceptJS.MainConfig} */
+exports.config = {
+  tests: './*_test.js',
+  output: './output',
+  helpers: {
+    Playwright: {
+      browser: 'chromium',
+      url: 'https://www.oranum.com/',
+      show: true
+    }
+  },
+  timeout: 60,
+  gherkin: {
+    features: './tests/features/*.feature',
+    steps: './tests/step_definitions/*.js',
+  },
+  include: {
+    I: './steps_file.js'
+  },
+  name: 'Akshaya-Byborg-CodeceptJS-Assignment'
+}
