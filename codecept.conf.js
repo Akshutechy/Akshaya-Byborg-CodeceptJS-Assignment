d42e63051c45f515664fb3129a779f4c9f13af27
akshutechy <akshaya.techy@gmail.com>
2024-11-17 23:22:56 +0530

    Added allure reporter integration

--- codecept.conf.js
+++ codecept.conf.js
@@ -2,13 +2,13 @@ const {
   setHeadlessWhen,
   setCommonPlugins
 } = require('@codeceptjs/configure');
+const enablePlugin = process.env.ENABLE_PLUGIN ==='true'
 // turn on headless mode when running with HEADLESS=true environment variable
 // export HEADLESS=true && npx codeceptjs run
 setHeadlessWhen(process.env.HEADLESS);
 
 // enable all common plugins https://github.com/codeceptjs/configure#setcommonplugins
 setCommonPlugins();
-
 /** @type {CodeceptJS.MainConfig} */
 exports.config = {
   tests: './*_test.js',
@@ -18,10 +18,11 @@ exports.config = {
       browser: 'chromium',
       url: 'https://www.oranum.com',
       show: true,
-      waitForTimeout: 60000
+      waitForTimeout: 60000,
+      timeout: 120000
     }
   },
-  timeout: 120000,
+  timeout: 120,
   gherkin: {
     features: './tests/features/*.feature',
     steps: './tests/step_definitions/*.js',
@@ -30,5 +31,29 @@ exports.config = {
     I: './steps_file.js',
     homePagePage: "./tests/pages/homePage.js",
   },
-  name: 'Akshaya-Byborg-CodeceptJS-Assignment'
+  name: 'Akshaya-Byborg-CodeceptJS-Assignment',
+  plugins: {
+    allure:{
+      enabled: true,
+      require: '@codeceptjs/allure-legacy',
+    },
+    screenshotOnFail: {
+      enabled: enablePlugin
+    },
+    tryTo: {
+      enabled: enablePlugin
+    },
+    retryFailedStep: {
+      enabled: enablePlugin
+    },
+    retryTo: {
+      enabled: enablePlugin
+    },
+    eachElement: {
+      enabled: enablePlugin
+    },
+    stepTimeout: {
+      enabled: enablePlugin
+   }
+  }
 }
